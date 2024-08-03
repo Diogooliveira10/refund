167a2125a3b814d443fffb6013978d98d7e8c77a
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2024-08-03 18:44:29 -0300

    Creating a new expense object

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const amount = document.getElementById("amount")
 const expense = document.getElementById("expense")
 const category = document.getElementById("category")
 
-// capture the input event to format the value
+// capture the input event to format the value.
 amount.oninput = () => {
 // gets the current value of the input and removes non-numeric characters.
     let value = amount.value.replace(/\D/g, "")
@@ -27,6 +27,20 @@ function formatCurrencyBRL(value) {
     return value
 }
 
+// captures the form's submit event to obtain the values.
 form.onsubmit = (event) => {
+    // prevents the default behavior of reloading the page.
     event.preventDefault()
+
+    // creates an object with the details on the new expense.
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        create_at: new Date(),
+    }
+
+    console.log(newExpense)
 }
